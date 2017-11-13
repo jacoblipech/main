85793d6f59420e45d60847f56f0c0ef80be399c0
Jacob Li Peng Cheng <JacobLI@223-93.priv23.nus.edu.sg>
2017-11-13 23:17:53 +0800

    add test case for add birthday command parser test

--- src/test/java/seedu/address/logic/parser/AddBirthdayCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddBirthdayCommandParserTest.java
@@ -5,7 +5,10 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_BIRTHDAY_AMY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_BIRTHDAY;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -32,5 +35,23 @@ public class AddBirthdayCommandParserTest {
     public void parse_invalidArgs_throwsParseException() {
         assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 AddBirthdayCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_birthdaySpecified_failure() throws Exception {
+        // no indexes
+        ArrayList<Index> indexes = new ArrayList<Index>();
+        final String birthday = "05/10/1994";
+        String userInput = " " + PREFIX_BIRTHDAY.toString() + birthday;
+
+        assertParseFailure(parser, userInput, MESSAGE_INVALID_INDEX);
+    }
+
+    @Test
+    public void parse_noFieldSpecified_failure() throws Exception {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddBirthdayCommand.MESSAGE_USAGE);
+
+        // nothing at all
+        assertParseFailure(parser, AddBirthdayCommand.COMMAND_WORDVAR_1, expectedMessage);
     }
 }
